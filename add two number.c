e504ff2a365750a568bfb3de3fb437534a89c598
Alaindeve1 <157726652+Alaindeve1@users.noreply.github.com>
2024-01-25 18:04:53 +0200

    Add files via upload

---
+++ add two number.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main (){
+ int num1, num2, sum;
+ printf("please Enter the first number: ");
+ scanf("%d", &num1);
+ printf("Please enter the second number :");
+ scanf("%d", &num2);
+ sum = num1 + num2;
+ printf("sum:%d", sum);
+ printf("/n");
+
+     return 0;
+
+
+}
